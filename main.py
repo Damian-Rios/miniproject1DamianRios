f30d6450102308ed949c17af656ec1a9c455e08b
Damian-Rios <damianjrios5@gmail.com>
2024-09-15 13:35:08 -0500

    seperated dates and closing prices into different functions. set my labels for the plots. put plot creation in for loop

--- main.py
+++ main.py
@@ -12,20 +12,28 @@ def get_closing(ticker):
     hist = stock.history(period="5d")
 
     closing_list = []
+
+    for price in hist['Close']:
+        closing_list.append(round(price, 2))
+
+    return closing_list
+
+def get_dates(ticker):
+    stock = yf.Ticker(ticker)
+    hist = stock.history(period="5d")
+
     closing_dates = []
 
     for date in hist.index:
         closing_dates.append(date.strftime('%Y-%m-%d'))
 
-    for price in hist['Close']:
-        closing_list.append(round(price, 2))
-
-    return closing_dates, closing_list
+    return closing_dates
 
 my_tickers = ["MSFT", "AAPL", "GOOG", "TSLA", "NTDOY"]
 
 for ticker in my_tickers:
-    closing_dates, closing_prices = get_closing(ticker)
+    closing_dates = get_dates(ticker)
+    closing_prices = np.array(get_closing(ticker))
 
     print(f"Ticker: {ticker}")
     print("Dates:\t\tClosing Prices:")
@@ -35,9 +43,22 @@ for ticker in my_tickers:
 
     print()
 
-my_tickers = np.array(my_tickers)
+    plt.figure()
 
-plt.plot(my_tickers)
-plt.xlabel("Ticker")
-plt.ylabel("Closing Price")
-plt.show()
+    # plots the graph
+    plt.plot(closing_dates, closing_prices)
+
+    # Get our min and max for y
+    low_price = min(closing_prices)
+    high_price = max(closing_prices)
+
+    # set our yaxis min and max
+    plt.ylim(low_price, high_price)
+
+    # set our labels for the graph
+    plt.xlabel("Date")
+    plt.ylabel("Closing Price")
+    plt.title(f"Closing Prices for {ticker}")
+
+    # Display the graph
+    plt.show()
