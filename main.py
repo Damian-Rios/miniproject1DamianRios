d969c4954e04615dd094f3e0138b3fcd1b05ee3f
Damian-Rios <damianjrios5@gmail.com>
2024-09-15 12:12:25 -0500

    updated get_closing function to also grab the dates matching the closing prices. added functionality to print information to console neatly

--- main.py
+++ main.py
@@ -12,14 +12,32 @@ def get_closing(ticker):
     hist = stock.history(period="5d")
 
     closing_list = []
+    closing_dates = []
+
+    for date in hist.index:
+        closing_dates.append(date.strftime('%Y-%m-%d'))
 
     for price in hist['Close']:
         closing_list.append(round(price, 2))
 
-    return closing_list
+    return closing_dates, closing_list
 
 my_tickers = ["MSFT", "AAPL", "GOOG", "TSLA", "NTDOY"]
 
 for ticker in my_tickers:
-    closing_prices = get_closing(ticker)
-    print(f"Ticker: {ticker} \tClosing Price: {closing_prices}")
+    closing_dates, closing_prices = get_closing(ticker)
+
+    print(f"Ticker: {ticker}")
+    print("Dates:\t\tClosing Prices:")
+
+    for i in range(len(closing_dates)):
+        print(f"{closing_dates[i]}\t{closing_prices[i]}")
+
+    print()
+
+my_tickers = np.array(my_tickers)
+
+plt.plot(my_tickers)
+plt.xlabel("Ticker")
+plt.ylabel("Closing Price")
+plt.show()
