1ee4a6fd1c5d1580cd7750f636c2cdd6515df5df
Damian-Rios <damianjrios5@gmail.com>
2024-09-14 19:35:07 -0500

    Setup yfinance to pull 5 tickers.

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
 ### INF601 - Advanced Programming in Python
 ### Damian Rios
 ### Mini Project 1
+import pprint
+import yfinance as yf
+
+mytickers = ["MSFT", "AAPL", "GOOG", "TSLA", "NTDOY"]
+
+mydata = {}
+
+mytickers.sort()
+
+for ticker in mytickers:
+    result = yf.Ticker(ticker)
+    mydata[ticker] = {'ticker': ticker,
+                      'dayHigh': result.info['dayHigh']
+                      }
+    #print(f"Ticker: {ticker} \tDaily High: {result.info['dayHigh']}")
+
+pprint.pprint(mydata)
